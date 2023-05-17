38f091075e7ea69c1a4ed6fb50093c56777d5e78
avitej-iyer <iyeravitej2@gmail.com>
2023-05-17 14:08:38 +0530

    modified ast, started parser

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,22 @@
+import "Splice-Core/token"
 package ast
 
 type Node interface{ TokenLiteral() string}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (x *Program) TokenLiteral() string{
+	if len(x.Statements) > 0{
+		return x.Statements[0].TokenLiteral()
+	}
+	else{
+		return ""
+	}
+}
+
+
 
 type Statement interface{ 
 	Node
@@ -11,3 +27,21 @@ type Expression interface{
 	Node
 	expressionNode()
 }
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func(ls *LetStatement) statementNode() {}
+func(ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
+
+type Identifier struct{
+	Token token.Token
+	Value string
+}
+
+func(i *Identifier) expressionNode() {}
+func(i *Identifier) TokenLiteral() string {return i.Token.Literal}
+
